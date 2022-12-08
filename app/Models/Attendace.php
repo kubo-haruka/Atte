c078b601c5556eb64812b54d63b998152011ab80
kubo-haruka <rupixx28@gmail.com>
2022-12-08 23:11:40 +0900

    model作成 #8

---
+++ app/Models/Attendace.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendace extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $fillable =
+    [
+        'user_id',
+        'date',
+        'start_time',
+        'end_time'
+    ];
+}
