509673d47043046e62788808dd92a8ce5e477ee7
kubo-haruka <112786056+kubo-haruka@users.noreply.github.com>
2022-12-19 20:09:20 +0900

    Revert "ルート作成 #13"

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterdUserController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\StampController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,14 +13,12 @@ use App\Http\Controllers\Auth\StampController;
 |
 */
 
-Route::get('/register', [RegisterdUserController::class, 'create']);
-Route::post('/register', [RegisterUserController::class, 'store']);
-Route::get('/login', [AuthenticatedSessionController::class, 'login']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth');
-Route::get('/home', [StampController::class, 'index'])->middleware('auth');
-Route::post('/home', [StampController::class, 'startAttendace'])->middleware('auth');
-Route::post('/home', [StampController::class, 'stopAttendace'])->middleware('auth');
-Route::post('/home', [StampController::class, 'startRest'])->middleware('auth');
-Route::post('/home', [StampController::class, 'stopRest'])->middleware('auth');
-Route::get('/attendace', [StampController::class, 'attendace']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
